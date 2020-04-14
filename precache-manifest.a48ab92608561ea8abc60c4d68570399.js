45c0926c2f0a72b97b4d78f5772b7214033d36a0
Inderpreet Kaur <inder05081995@gmail.com>
2020-04-15 00:20:33 +0530

    Updates

---
+++ precache-manifest.a48ab92608561ea8abc60c4d68570399.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e71665c37088871d0b312dd5dd819623",
+    "url": "/time-table-react/index.html"
+  },
+  {
+    "revision": "9ab7ddf277eab21b8450",
+    "url": "/time-table-react/static/css/2.a43b59a1.chunk.css"
+  },
+  {
+    "revision": "0b0b54df7e3f80826906",
+    "url": "/time-table-react/static/css/main.d90930cb.chunk.css"
+  },
+  {
+    "revision": "9ab7ddf277eab21b8450",
+    "url": "/time-table-react/static/js/2.7651f536.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/time-table-react/static/js/2.7651f536.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b0b54df7e3f80826906",
+    "url": "/time-table-react/static/js/main.85becd2a.chunk.js"
+  },
+  {
+    "revision": "eed34997ff7397f19f06",
+    "url": "/time-table-react/static/js/runtime-main.754aa69e.js"
+  }
+]);
